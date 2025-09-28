ec2f6064d7ebce70630491a7a6ed4ca01e303325
Git_Karina <karinkakarik@mail.ru>
2025-09-28 21:08:17 +0300

    Task 4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Student:
+    all_students = []
 
     def __init__(self, name, surname, gender):
         self.name = name
@@ -7,6 +8,23 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        Student.all_students.append(self)
+
+    def mean_in_total(course, students = all_students):
+        '''
+        :param course: course
+        :param students: all of students on this course
+        :return: mean in total for this course
+
+        The function finds the mean in total of the course
+
+        '''
+        total_grade = []
+        for student in students:
+            for course_pr in student.courses_in_progress:
+                if course == course_pr:
+                    total_grade += student.grades[course]
+        return f"На курсе '{course}' средний балл по всем студентам {sum(total_grade) / len(total_grade)}"
 
     def rate_lecturer(self, lecturer, course, grade):
 
@@ -50,6 +68,15 @@ class Student:
         mean = sum(all_grades) / len(all_grades)
         return mean
 
+    def __gt__(self, other):
+        return self.__mean > other.__mean
+
+    def __lt__(self, other):
+        return self.__mean < other.__mean
+
+    def __eq__(self, other):
+        return self.__mean == other.__mean
+
     def __str__(self):
         """
 
@@ -83,10 +110,28 @@ class Mentor:
 
 
 class Lecturer(Mentor):
+    all_lecturers = []
+
     def __init__(self, name, surname):
         self.grades = {}
+        Lecturer.all_lecturers.append(self)
         super().__init__(name, surname)
 
+    def mean_in_total(course, lecturers = all_lecturers):
+        '''
+        :param course: course
+        :param lecturers: all of lecturers on this course
+        :return: mean in total for this course
+
+        The function finds the mean in total of the course
+
+        '''
+        total_grade = []
+        for lecturer in lecturers:
+            for course_pr in lecturer.courses_attached:
+                if course == course_pr:
+                    total_grade += lecturer.grades[course]
+        return f"На курсе '{course}' средний балл по всем лекторам {sum(total_grade) / len(total_grade)}"
     @property
     def __mean(self):
         """
@@ -100,6 +145,15 @@ class Lecturer(Mentor):
             all_grades += grades
         mean = sum(all_grades) / len(all_grades)
         return mean
+
+    def __gt__(self, other):
+        return self.__mean > other.__mean
+
+    def __lt__(self, other):
+        return self.__mean < other.__mean
+
+    def __eq__(self, other):
+        return self.__mean == other.__mean
 
     def __str__(self):
         """
@@ -162,25 +216,38 @@ best_student.courses_in_progress += ['Python']
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 
+bad_lecturer = Lecturer('Misha', 'Bad')
+bad_lecturer.courses_attached += ['Python']
 cool_lecturer = Lecturer('Donald', 'Duck')
 cool_lecturer.courses_attached += ['Python']
 
 best_student.rate_lecturer(cool_lecturer, 'Python', 4)
 best_student.rate_lecturer(cool_lecturer, 'Python', 9)
 best_student.rate_lecturer(cool_lecturer, 'Python', 3)
-best_student.rate_lecturer(cool_lecturer, 'Python', 7)
+best_student.rate_lecturer(bad_lecturer, 'Python', 7)
 best_student.rate_lecturer(cool_lecturer, 'Python', 40)
 
-#worst_student = Student('Andrew', 'Tate', 'male')
-#worst_student.courses_in_progress += ['Python']
+worst_student = Student('Andrew', 'Tate', 'male')
+worst_student.courses_in_progress += ['Python']
 
 
-cool_reviewer.rate_hw(best_student, 'Python', 2)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(worst_student, 'Python', 2)
+cool_reviewer.rate_hw(worst_student, 'Python', 1)
+cool_reviewer.rate_hw(worst_student, 'Python', 1)
+
+mean1 = Student.mean_in_total('Python')
+mean2 = Lecturer.mean_in_total('Python')
 
 print(best_student.grades)
+print(worst_student.grades)
 print(cool_lecturer.grades)
 print(cool_reviewer)
 print(cool_lecturer)
 print(best_student)
+print(mean1)
+print(mean2)
+print(best_student > worst_student)
+print(cool_lecturer > bad_lecturer)
